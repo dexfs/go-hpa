4f230720409972b0ddf5fb32e33bfb15fe4c8840
Andre Santos <andre@dexdev.com.br>
2020-06-13 03:41:26 -0300

    feat: k8s gcp

--- src/squareroot/main.go
+++ src/squareroot/main.go
@@ -1,7 +1,13 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
 
 func main() {
-	fmt.Println(squareroot())
+	http.HandleFunc("/", func (w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, squareroot())
+	})
+	http.ListenAndServe(":8000", nil)
 }
